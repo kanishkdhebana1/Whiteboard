85e9e7bdbdea11fb7d50936c60616d6e37e517c5
kanishkdhebana <dhebanakanishk@gmail.com>
2024-08-16 10:36:31 +0530

    Working

--- app/src/main/java/com/example/whiteboard/MainActivity.kt
+++ app/src/main/java/com/example/whiteboard/MainActivity.kt
@@ -3,14 +3,17 @@ package com.example.whiteboard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import com.example.whiteboard.ui.WhiteBoardScreen
 import com.example.whiteboard.ui.WhiteBoardViewModel
 
 class MainActivity : ComponentActivity() {
+    private val viewModel by viewModels<WhiteBoardViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            WhiteBoardScreen(viewModel = WhiteBoardViewModel())
+            WhiteBoardScreen(viewModel = viewModel)
         }
     }
 }
